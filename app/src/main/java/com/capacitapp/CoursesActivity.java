2920b4b5157e2b2ee2cf61823997085bef683875
nelsonvilte <nelsonvilte@gmail.com>
2024-05-13 04:42:05 -0300

    actualizados CourseActivity.java y MainActivity.java

--- app/src/main/java/com/capacitapp/CoursesActivity.java
+++ app/src/main/java/com/capacitapp/CoursesActivity.java
@@ -13,12 +13,11 @@ public class CoursesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_courses);
+        //setContentView(R.layout.activity_courses);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-}
 }
