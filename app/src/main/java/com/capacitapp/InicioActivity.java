39c62f5190521fdb946e69f9a07b62bef41af17a
Metalderly <osvaldoeverdiaz@gmail.com>
2024-05-14 04:11:31 -0300

    adelante un poco mas en las vistas y su actividad

--- app/src/main/java/com/capacitapp/InicioActivity.java
+++ app/src/main/java/com/capacitapp/InicioActivity.java
@@ -2,6 +2,10 @@ package com.capacitapp;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,13 +16,44 @@ public class InicioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inicio);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        //EdgeToEdge.enable(this);
+             //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        //    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+         //   return insets;
+        //}
+
+        TextView iniciarSesionTextView = findViewById(R.id.textView7);
+        TextView registrarseTextView = findViewById(R.id.textView9);
+        TextView contactanosTextView = findViewById(R.id.textView11);
+        iniciarSesionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicioActivity.this, loginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        registrarseTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicioActivity.this, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        contactanosTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicioActivity.this, ContactoActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
