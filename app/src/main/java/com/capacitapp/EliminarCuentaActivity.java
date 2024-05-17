a047a6f77e6668459e1d95e0f6e88cd4a5a25d20
Edith Moyano <moyanoe357@gmail.com>
2024-05-17 14:32:13 -0300

    configuro retroceso con flecha back de toolbar, de Eliminarcuenta a Perfil

--- app/src/main/java/com/capacitapp/EliminarCuentaActivity.java
+++ app/src/main/java/com/capacitapp/EliminarCuentaActivity.java
@@ -1,6 +1,9 @@
 package com.capacitapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EliminarCuentaActivity extends AppCompatActivity {
 
+    private ImageView imgBackArrow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_eliminar_cuenta);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        imgBackArrow = findViewById(R.id.btn_back);
+
+        imgBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EliminarCuentaActivity.this, PerfilActivity.class);
+                startActivity(intent);
+
+            }
         });
     }
 }
