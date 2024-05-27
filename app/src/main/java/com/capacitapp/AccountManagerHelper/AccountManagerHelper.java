effaf96d7a6872f24d23caff1ea2c45892548c30
Metalderly <osvaldoeverdiaz@gmail.com>
2024-05-27 15:59:08 -0300

    poniendo los ultimos arreglos

--- app/src/main/java/com/capacitapp/AccountManagerHelper/AccountManagerHelper.java
+++ app/src/main/java/com/capacitapp/AccountManagerHelper/AccountManagerHelper.java
@@ -24,7 +24,7 @@ public class AccountManagerHelper {
     }
 
     public void createAccount(String useremail, String password) {
-        // Comprueba si el usuario ya tiene una cuenta
+
         Account[] accounts = accountManager.getAccountsByType("account_type");
         if (accounts.length == 0) {
             Account account = new Account(useremail, "account_type");
@@ -38,16 +38,16 @@ public class AccountManagerHelper {
             Account account = accounts[0];
             accountManager.getAuthToken(account, "auth_token_type", null, false, callback, null);
         } else {
-            // No hay cuentas, manejar el caso
+
             callback.run(null);
         }
     }
 
     public void login(final Context context, String username, String password) {
-        // Crea una nueva cuenta
+
         createAccount(username, password);
 
-        // Obtén el token de autenticación
+
         getAuthToken(context, new AccountManagerCallback<Bundle>() {
             @Override
             public void run(AccountManagerFuture<Bundle> future) {
@@ -55,17 +55,17 @@ public class AccountManagerHelper {
                     Bundle result = future.getResult();
                     String token = result.getString(AccountManager.KEY_AUTHTOKEN);
                     if (token != null) {
-                        // Inicia la actividad principal
+
                         Intent intent = new Intent(context, MainActivity.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         context.startActivity(intent);
                     } else {
-                        // Muestra un mensaje de error
+
                         Toast.makeText(context, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
                     }
                 } catch (OperationCanceledException | AuthenticatorException | IOException e) {
                     e.printStackTrace();
-                    // Manejar el error adecuadamente
+
                     Toast.makeText(context, "Error al obtener el token de autenticación", Toast.LENGTH_SHORT).show();
                 }
             }
