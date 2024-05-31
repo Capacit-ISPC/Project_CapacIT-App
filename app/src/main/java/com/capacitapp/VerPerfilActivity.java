6381bdad591365c68311cdeb4ab29feca9001a4e
Edith Moyano <moyanoe357@gmail.com>
2024-05-31 20:47:31 -0300

    agrego listview a verPerfil

--- app/src/main/java/com/capacitapp/VerPerfilActivity.java
+++ app/src/main/java/com/capacitapp/VerPerfilActivity.java
@@ -1,10 +1,14 @@
 package com.capacitapp;
 
+
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +16,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.capacitapp.DBHelper.DBHelper;
+import com.capacitapp.models.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VerPerfilActivity extends AppCompatActivity {
 
     private ImageView imgBackArrow;
-
     private Button btn;
+
+
+
+    ListView VerPerfil;
+    DBHelper conn;
+    List<String> item =null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,14 @@ public class VerPerfilActivity extends AppCompatActivity {
 
         imgBackArrow = findViewById(R.id.btn_back);
         btn = findViewById(R.id.btn_ireditarperfil);
+
+
+
+        VerPerfil = findViewById(R.id.lv_verdatosperfil);
+        showUser();
+        conn = new DBHelper(getApplicationContext());
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,4 +70,12 @@ public class VerPerfilActivity extends AppCompatActivity {
         });
 
     }
+
+    private void showUser() {
+
+    }
+
+
+
+
 }
