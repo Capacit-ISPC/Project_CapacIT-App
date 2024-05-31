aa2252b36d8608888c6992331469ad7120212a64
Edith Moyano <moyanoe357@gmail.com>
2024-05-31 17:06:31 -0300

    configuro boton cancelar de activity eliminar cuenta para retroceso a activity anterior

--- app/src/main/java/com/capacitapp/EliminarCuentaActivity.java
+++ app/src/main/java/com/capacitapp/EliminarCuentaActivity.java
@@ -3,6 +3,7 @@ package com.capacitapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,8 @@ public class EliminarCuentaActivity extends AppCompatActivity {
 
     private ImageView imgBackArrow;
 
+    private Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,16 @@ public class EliminarCuentaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_eliminar_cuenta);
 
         imgBackArrow = findViewById(R.id.btn_back);
+        btn = findViewById(R.id.btn_cancelar_eliminar);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EliminarCuentaActivity.this, PerfilActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         imgBackArrow.setOnClickListener(new View.OnClickListener() {
             @Override
