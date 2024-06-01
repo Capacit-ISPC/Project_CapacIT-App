81faf4dd6f80a0bea9f03c853e9c4db033c70e56
nelsonvilte <nelsonvilte@gmail.com>
2024-06-01 03:06:40 -0300

    actualizando logica para mostrar los cursos registrados

--- app/src/main/java/com/capacitapp/fragments/MisCursosFragment.java
+++ app/src/main/java/com/capacitapp/fragments/MisCursosFragment.java
@@ -1,45 +1,49 @@
 package com.capacitapp.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.capacitapp.DBHelper.DBHelper;
 import com.capacitapp.R;
 import com.capacitapp.adapters.MisCursosAdapter;
-import com.capacitapp.models.ItemData;
+import com.capacitapp.models.Curso;
+import com.capacitapp.utils.UserPreferences;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MisCursosFragment extends Fragment {
     private RecyclerView recyclerView;
     private MisCursosAdapter adapter;
-    private List<ItemData> itemList;
+    private DBHelper dbHelper;
+    private Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();
+        dbHelper = new DBHelper(context);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mis_cursos, container, false);
-
         recyclerView = view.findViewById(R.id.recyclerViewMisCursos);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
-        // Aquí deberías crear y poblar itemList con tus datos
-        itemList = new ArrayList<>();
-        List<String> checkBoxItems1 = Arrays.asList("Clase 1", "Clase 2", "Clase 3");
-        List<String> checkBoxItems2 = Arrays.asList("Clase 1", "Clase 2");
-        itemList.add(new ItemData("Angular", checkBoxItems1));
-        itemList.add(new ItemData("Django", checkBoxItems2));
-        // Agrega más elementos según sea necesario
+        int userId = UserPreferences.getLoggedUserId(context); // Obtén el ID del usuario logueado
+        List<Curso> cursos = dbHelper.getCursosByUserId(userId); // Obtén la lista de cursos
 
-        adapter = new MisCursosAdapter(itemList);
+        adapter = new MisCursosAdapter(context, cursos);
         recyclerView.setAdapter(adapter);
 
         return view;
