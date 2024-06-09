9a6fa936a3c1f96784d9906e093d0938cc166436
nelsonvilte <nelsonvilte@gmail.com>
2024-06-08 23:11:29 -0300

    actualizando logica eliminar cuenta

--- app/src/main/java/com/capacitapp/EliminarCuentaActivity.java
+++ app/src/main/java/com/capacitapp/EliminarCuentaActivity.java
@@ -8,18 +8,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.capacitapp.DBHelper.DBHelper;
+import com.capacitapp.utils.UserPreferences;
 
 public class EliminarCuentaActivity extends AppCompatActivity {
 
     private ImageView imgBackArrow;
 
     private Button btnCancelar, btnEliminarCuenta;
+    private RadioButton radioButtonEliminar;
     private DBHelper dbHelper;
     private int userId;;
 
@@ -32,13 +35,17 @@ public class EliminarCuentaActivity extends AppCompatActivity {
         imgBackArrow = findViewById(R.id.btn_back);
         btnCancelar = findViewById(R.id.btn_cancelar_eliminar);
         btnEliminarCuenta = findViewById(R.id.btn_eliminar_cuenta);
+        radioButtonEliminar = findViewById(R.id.radioButton_eliminar);
 
         dbHelper = new DBHelper(this);
-        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        userId = prefs.getInt("userId", -1);
+        //SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        //userId = prefs.getInt("userId", -1);
         //userId = getIntent().getIntExtra("userId", 0);
+        userId = UserPreferences.getLoggedUserId(this);
 
         Log.d("EliminarCuentaActivity", "ID del usuario: " + userId);
+        btnEliminarCuenta.setEnabled(false); // Desactivar botón eliminar al inicio
+
 
         btnCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,16 +67,18 @@ public class EliminarCuentaActivity extends AppCompatActivity {
         btnEliminarCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-                int userId = prefs.getInt("userId", -1);
+                //SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                //int userId = prefs.getInt("userId", -1);
 
                 if (userId != -1) {
 
                     dbHelper.deleteUser(userId);
 
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.remove("userId");
-                    editor.apply();
+                    //SharedPreferences.Editor editor = prefs.edit();
+                    //editor.remove("userId");
+                    //editor.apply();
+
+                    UserPreferences.clearUserPreferences(EliminarCuentaActivity.this);
 
                     Toast.makeText(EliminarCuentaActivity.this, "Cuenta eliminada con éxito", Toast.LENGTH_SHORT).show();
 
